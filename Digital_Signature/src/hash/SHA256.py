65310e26677b73985108151a39c3fcf43898de1f
Your Name <Triet.LM234040@sis.hust.edu.vn>
2026-04-13 13:58:20 +0700

    cap nhat cac ham rotate, lower va upper

--- Digital_Signature/src/hash/SHA256.py
+++ Digital_Signature/src/hash/SHA256.py
@@ -2,3 +2,15 @@ def to_32_bit (n):
     return n & 0xFFFFFFFF
 def rotate_right (n, d):
     return to_32_bit((n >> d) | (n << (32 - d)))
+def choose (x, y, z):
+    return (x & y) ^ (~x & z)
+def majority (x, y, z):
+    return (x & y) ^ (x & z) ^ (y & z)
+def uppercase_sigma0 (x):
+    return rotate_right(x, 2) ^ rotate_right(x, 13) ^ rotate_right(x, 22)
+def uppercase_sigma1 (x):
+    return rotate_right(x, 6) ^ rotate_right(x, 11) ^ rotate_right(x, 25)
+def lowercase_sigma0 (x):
+    return rotate_right(x, 7) ^ rotate_right(x, 18) ^ (x >> 3)
+def lowercase_sigma1 (x):
+    return rotate_right(x, 17) ^ rotate_right(x, 19) ^ (x >> 10)
